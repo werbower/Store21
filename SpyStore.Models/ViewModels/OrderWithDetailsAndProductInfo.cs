a6515f97afd5f639e0df0dae8356182a18c87e23
werbower <werbower@gmail.com>
2020-01-13 14:59:05 +0200

    view models

---
+++ SpyStore.Models/ViewModels/OrderWithDetailsAndProductInfo.cs
@@ -0,0 +1,36 @@
+﻿using AutoMapper;
+using SpyStore.Models.Entities;
+using SpyStore.Models.Entities.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SpyStore.Models.ViewModels
+{
+    public class OrderWithDetailsAndProductInfo: OrderBase
+    {
+        private static readonly MapperConfiguration _mapperCfg;
+
+        public Customer Customer { get; set; }
+        public IList<OrderDetailWithProductInfo> OrderDetails { get; set; }
+
+        static OrderWithDetailsAndProductInfo()
+        {
+            _mapperCfg = new MapperConfiguration(cfg =>
+            {
+                cfg.CreateMap<Order, OrderWithDetailsAndProductInfo>()
+                    .ForMember(r => r.OrderDetails, y => y.Ignore());
+            });
+        }
+
+        public static OrderWithDetailsAndProductInfo Create(
+            Order order, Customer customer, IEnumerable<OrderDetailWithProductInfo> details)
+        {
+            var viewModel = _mapperCfg.CreateMapper().Map<OrderWithDetailsAndProductInfo>(order);
+            viewModel.OrderDetails = details.ToList();
+            viewModel.Customer = customer;
+            return viewModel;
+        }
+    }
+}
