4f0d036aec0b86dd2195324d1bebf791060a37f7
UserDev <UserDev>
2020-01-19 20:37:23 +0200

    exceptions

---
+++ SpyStore.Dal/Exceptions/SpyStoreRetryLimitExceededException.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SpyStore.Dal.Exceptions
+{
+    public class SpyStoreRetryLimitExceededException : SpyStoreException
+    {
+        public SpyStoreRetryLimitExceededException()
+        {
+        }
+
+        public SpyStoreRetryLimitExceededException(string message) : base(message)
+        {
+        }
+
+        public SpyStoreRetryLimitExceededException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+    }
+}
---
+++ SpyStore.Dal/Repos/Base/IRepo.cs
@@ -0,0 +1,11 @@
+﻿using SpyStore.Models.Entities.Base;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SpyStore.Dal.Repos.Base
+{
+    public interface IRepo<T>: IDisposable where T: EntityBase
+    {
+    }
+}
